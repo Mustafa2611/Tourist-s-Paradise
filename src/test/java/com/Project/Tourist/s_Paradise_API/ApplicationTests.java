c0d8a2b177af260136ef18c01b1932e62747522a
Mustafa <Mustafa.rs2018@gmail.com>
2025-01-22 20:18:55 +0200

    Initial

---
+++ src/test/java/com/Project/Tourist/s_Paradise_API/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.Tourist.s_Paradise_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
